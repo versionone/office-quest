f38598858f40ca15f76af3e771ce75c6e3ae3178
mickeyrogers <mickeyrogers123@gmail.com>
2018-07-19 05:29:32 -0400

    Add some error handling to express, bring in HttpModule, and use HttpClient to hit a test endpoint and log the response or error

--- api/src/app.js
+++ api/src/app.js
@@ -1,8 +1,38 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+
+let setHeaders = function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Content-Type', 'application/json');
+    next();
+};
+
+const logError = (err, req, res, next) => {
+    console.error(err.stack);
+    next(err)
+};
+
+const errorHandler = (err, req, res, next) => {
+    res.status(500);
+    res.render('error', { error: err })
+};
+
+const clientErrorHandler = (err, req, res, next) => {
+    if (req.xhr) {
+        res.status(500).send({ error: err })
+    } else {
+        next(err)
+    }
+};
+
+app.use(setHeaders);
+app.use(logError);
+app.use(clientErrorHandler);
+app.use(errorHandler);
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.json({"data": "it worked"})
 });
 
 app.listen(4201);
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Hackweek Quest';
+  constructor(private http:HttpClient) {
+
+  }
+
+  testApi() {
+    this.http.get("http://localhost:4201/").subscribe(response => {
+      console.log("data", response);
+    }, error => {
+      console.log("error", error);
+    })
+  }
 }
