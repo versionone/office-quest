f104be709fa304399b0ddd8c9b60099c33b8ffe5
mickeyrogers <mickeyrogers123@gmail.com>
2018-07-23 06:40:32 -0400

    Inject forms module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
@@ -11,9 +12,11 @@ import { JoinComponent } from './quests/join/join.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
