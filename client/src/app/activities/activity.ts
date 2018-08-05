ec9008acd4abb29e9bc151a7c4dc6eb305065180
mickeyrogers <mickeyrogers123@gmail.com>
2018-08-04 20:13:23 -0400

    Remove unused enums and props

--- client/src/app/activities/activity.ts
+++ client/src/app/activities/activity.ts
@@ -1,23 +1,12 @@
 export class Activity {
   _id: string;
   quest_id: string;
-  participant_id: string;
-  state: ActivityState;
   type: number;
   start_datetime: Date;
-  completion_type: CompletionType;
   message: string;
   answer: any;
   faux: any;
   email: Email;
-}
-
-enum ActivityState {
-  FUTURE = 0,
-  STAGED = 32,
-  NOTIFYING = 48,
-  ACTIVE = 64,
-  COMPLETE = 128,
 }
 
 export enum ActivityType {
@@ -26,13 +15,6 @@ export enum ActivityType {
   Scramble = 2,
   Hunt = 3,
 }
-
-enum CompletionType {
-  Answer = 0,
-  Manual = 1,
-  Automatic = 2,
-}
-
 class Email {
   subject: string;
   message: string;
