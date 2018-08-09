75ef3ad62e525161c437b1c4121ee599de1f9ce4
mickeyrogers <mickeyrogers123@gmail.com>
2018-08-09 11:13:12 -0400

    Add login and admin components

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
