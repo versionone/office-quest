cf1d07c1eb0f47bb12ea25f65d8f355908b9da24
mickeyrogers <mickeyrogers123@gmail.com>
2018-08-10 05:49:51 -0400

    Parse back to JSON when retrieved

--- admin/src/app/storage.service.ts
+++ admin/src/app/storage.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class StorageService {
 
   public getCredentials(): string {
-    return localStorage.getItem("credentials");
+    return JSON.parse(localStorage.getItem("credentials"));
   }
 
   public setCredentials(credentials): void {
