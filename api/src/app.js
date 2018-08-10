87d2e17b6d0e11cb9371f6e6f6f647aaefb0caf4
mickeyrogers <mickeyrogers123@gmail.com>
2018-08-10 05:48:34 -0400

    Allow email and password headers for authorization from the client

--- api/src/app.js
+++ api/src/app.js
@@ -24,7 +24,7 @@ const CompletionType = {
 
 let setHeaders = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Email, Password');
     res.header('Content-Type', 'application/json');
     next();
 };
