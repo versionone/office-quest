bd4a9b37667ebb05094c71b19e514412e99b5e14
mickeyrogers <mickeyrogers123@gmail.com>
2018-08-12 15:40:17 -0400

    Root url takes admin to manual approvals

--- admin/src/app/app-routing.module.ts
+++ admin/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { TriviaComponent } from './admin/trivia/trivia.component';
 import { ManualApprovalComponent } from './admin/manual-approval/manual-approval.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/admin', pathMatch: 'full' },
+  { path: '', redirectTo: '/manualApproval', pathMatch: 'full' },
   { path: 'manualApproval', component: ManualApprovalComponent },
   { path: 'trivia', component: TriviaComponent },
 ];
